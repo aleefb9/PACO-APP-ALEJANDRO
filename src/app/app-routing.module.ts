2b8cab9ef32dc9aab5d5004688b64d6c911fc57f
Jorge Ortega <jorgeortegarivera.guadalupe@alumnado.fundacionloyola.net>
2023-02-27 09:42:55 +0100

    reto1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { FavoritasComponent } from './favoritas/favoritas.component';
+import { CreditosComponent } from './creditos/creditos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	{path: 'home', component: HomeComponent},
+	{path: 'peliculas', component: PeliculasComponent},
+	{path: 'favoritas', component: FavoritasComponent},
+	{path: 'creditos', component: CreditosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
